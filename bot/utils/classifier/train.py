e31a920008b178dccfc535343d46253f3263c36a
marsolmos <m.marsolmos@gmail.com>
2021-08-14 16:59:03 +0200

    Basic layout for gpu-training

--- bot/utils/classifier/train.py
+++ bot/utils/classifier/train.py
@@ -3,6 +3,10 @@ import os
 import zipfile
 import matplotlib.pyplot as plt
 
+# Don't display general information messages from tf-gpu
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import RMSprop
@@ -15,6 +19,9 @@ base_dir = "D:/Data Warehouse/plantabit/0_rawdata"
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
 
+# Define GPU usage for training
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0],True)
 
 # Adding rescale, rotation_range, width_shift_range, height_shift_range,
 # shear_range, and zoom_range to our ImageDataGenerator
@@ -34,7 +41,7 @@ val_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
         train_dir,  # This is the source directory for training images
         target_size=(150, 150),  # All images will be resized to 150x150
-        batch_size=1,
+        batch_size=32,
         seed=seed,
         class_mode='categorical')
 
@@ -43,7 +50,7 @@ train_generator = train_datagen.flow_from_directory(
 validation_generator = val_datagen.flow_from_directory(
         validation_dir,
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=32,
         class_mode='categorical')
 
 # Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
@@ -75,21 +82,21 @@ x = layers.Dense(512, activation='relu')(x)
 x = layers.Dropout(0.5)(x)
 
 # Create output layer with a single node and sigmoid activation
-output = layers.Dense(9, activation='softmax')(x)
+output = layers.Dense(12, activation='softmax')(x)
 
 # Configure and compile the model
 model = Model(img_input, output)
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(lr=0.001),
+              optimizer=RMSprop(learning_rate=0.001),
               metrics=['acc'])
 
-history = model.fit_generator(
+history = model.fit(
       train_generator,
       steps_per_epoch=100,
-      epochs=30,
+      epochs=5,
       validation_data=validation_generator,
       validation_steps=50,
-      verbose=2)
+      verbose=1)
 
 print('\nSAVING MODEL!\n')
 save_name = 'models/{}'.format(MODEL_NAME)
