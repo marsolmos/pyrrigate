b8ea6a9425a3bd1012ea8224dc6f4b21badae116
marsolmos <m.marsolmos@gmail.com>
2021-08-15 16:04:12 +0200

    Add confusion matrix to evaluation pipeline

--- bot/utils/classifier/evaluate.py
+++ bot/utils/classifier/evaluate.py
@@ -1,6 +1,7 @@
 '''Evaluation Script'''
 import json
 import math
+import numpy as np
 import os
 import pandas as pd
 import pickle
@@ -13,9 +14,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
 
 
 scores_file = "utils/classifier/dvc_objects/test_scores.json"
+confusion_file = "utils/classifier/dvc_objects/confusion.json"
 prc_file = "utils/classifier/dvc_objects/prc.json"
 roc_file = "utils/classifier/dvc_objects/roc.json"
 
@@ -41,6 +44,13 @@ dataset = params["dataset"]
 base_dir = os.path.join("D:/Data Warehouse/plantabit", dataset)
 test_dir = os.path.join(base_dir, 'test')
 
+# Create mapping dict for test classes
+map_classes = {}
+index = 0
+for i in os.listdir(test_dir):
+    map_classes[index] = i
+    index += 1
+
 # Note that the test data should not be augmented!
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -59,3 +69,25 @@ test_acc = results[1]
 
 with open(scores_file, "w") as fd:
     json.dump({"test_loss": test_loss, "test_acc": test_acc}, fd, indent=4)
+
+# Save Confusion Matrix
+Y_pred = model.predict(test_generator)
+y_pred = np.argmax(Y_pred, axis=1)
+y_true = test_generator.classes
+# Map classes with class_dict
+y_pred = [map_classes[k] for k in y_pred]
+y_true = [map_classes[k] for k in y_true]
+
+
+confusion_points = list(zip(y_true, y_pred))
+with open(confusion_file, "w") as fd:
+    json.dump(
+        {
+            "confusion": [
+                {"actual": y_true, "predicted": y_pred}
+                for y_true, y_pred in confusion_points
+            ]
+        },
+        fd,
+        indent=4,
+    )
