7d4282ea47137d4522d02efed8d8ba4bd271cfb4
marsolmos <m.marsolmos@gmail.com>
2021-08-28 15:43:00 +0200

    Update requirements.txt and .gitignore

--- bot/app.py
+++ bot/app.py
@@ -19,7 +19,7 @@ import os
 import logging
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from utils.download_images import downloadimages
+from utils.dataset.download_images import downloadimages
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
